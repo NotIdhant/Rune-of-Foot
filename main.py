c7c83dfee3192cf9843fc2ef900172f2974e306f
NotIdhant <idhantgupta03@gmail.com>
2026-09-15 18:09:54 -0400

    Initialize pygame and display greeting message
    Added pygame initialization and text rendering.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import pygame
+pygame.init()
+
 print("Greetings, traveler. Please state your name.")
 name = input(">> ")
+print(f"{name}. It has... a presence to it. Like the smell of petrichor, or perhaps - the stench of a sweaty athlete's foot.")
+wait(5)
+font = pygame.font.SysFont("Arial", 60)
+text_surface = font.render("RUNE OF FOOT", True, (255, 255, 255))
+print("Created by Idhant")
